e68073ae324f0efe9f0f113300aab316419a8898
Francisco Felipe Riffel <francisco.riffel@unochapeco.edu.br>
2020-11-24 00:36:10 -0300

    operaçoes de forma simples e pouco eficaz

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ɵConsole } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,16 +7,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Calculadora';
-  result: any;
-  fields = ["AC", "+/-", "%", "/", "7","8", "9", "X","4", "5", "6", "-", "1", "2", "3", "+","0", "."]
+  result: any = "";
+  value1: number = 0
+  value2: number = 0
+  fields = [" AC ", " +/- ", " % ", " / ", "7","8", "9", " X ","4", "5", "6", " - ", "1", "2", "3", " + ","0", "."];
+  operadores = ["%", "/", "X", "-", "+", "."];
 
 
   getField(field: string){
-    if(field == 'AC'){
-      this.result = 0
-    }else{
-      this.result = field
-    }
+    field == "AC"? this.Clear() : this.result += field;
+  }
 
+  calculate(){
+      var calculo = this.result.split(" ")
+      console.log(calculo)
+      this.value1 = Number(calculo[0]);
+      this.value2 = Number(calculo[2]);
+      switch(calculo[1]){
+        case "+":
+          this.result = this.value1 + this.value2;
+          break
+        case "-":
+          this.result = this.value1 - this.value2;
+          break;
+        case "X":
+          this.result = this.value1 * this.value2;
+          break;
+        case "/":
+          this.result = this.value1 / this.value2;
+          break;
+      }
+  }
+
+  private Clear(){
+    this.result = "";
+  }
+
+  private setNegativo(){
+    this.result > 0 ? this.result : null;
   }
 }
